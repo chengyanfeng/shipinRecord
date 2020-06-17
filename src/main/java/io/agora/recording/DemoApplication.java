985ee1dabaf5504963ba25b5a4fdaf0ec68e8501
chengyanfeng <chengyanfeng@163.com>
2020-06-17 15:46:45 +0800

    恢复和线上一模一样的代码

--- src/main/java/io/agora/recording/DemoApplication.java
+++ src/main/java/io/agora/recording/DemoApplication.java
@@ -19,36 +19,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    //以下是springboot的同时启用http和https 协议
-  /*  @Bean
-    public TomcatServletWebServerFactory servletContainer() { //springboot2 新变化
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-
-            @Override
-            protected void postProcessContext(Context context) {
-
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-        return tomcat;
-    }
-
-    private Connector initiateHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(8089);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }*/
-
-
 }
